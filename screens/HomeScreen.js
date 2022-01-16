7def1073d5441443d3d85df3f257a9bad2f9e19b
vjo001 <89363371+vjo001@users.noreply.github.com>
2022-01-16 10:18:29 -0500

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import {View,Text,TouchableOpacity,StyleSheet,Button} from 'react-native;
+import db from '../config';
+
+export default class HomeScreen extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      all_students: [],
+      presentPressedList: [],
+      absentPressedList: [],
+    };
+  }
+
+  componentDidMount = async() => {
+    var class_ref =await db.ref('/').on('value', data => {
+      var all_students =  []
+      var class_a = data.val();
+      for (var i in class_a) {
+        all_students.push(class_a[i]);
+      }
+      all_students.sort(function(a, b) {
+        return a.roll_no - b.roll_no;
+      });
+      this.setState({ all_students: all_students });
+      console.log(all_students);
+    });
+  }
+
+  updateAttendance(roll_no, status) {
+    var id ='';
+    if (roll_no <= 9) {
+      if = '0' + roll_no;
+    } else {
+      id = roll_no;
+    }
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm
+    }
+    today = dd + '-' + mm + '-' + yyyy;
+    var ref_path = id;
+    var class_ref = db.ref(ref_path);
+    class_ref.update({
+      [today]: status,
+    });
+  }
+
+
+
+
+
+goToSummary = ()=>{
+    this.props.navigation.navigate('SummaryScreen')
+  }
+
+
+render(){
+  var all_students
+}
